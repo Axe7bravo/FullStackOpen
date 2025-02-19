feac9377b28d70ff31fe431417e6c65286eb50ac
Thato Nkoko <thato.dice@gmail.com>
2025-02-19 18:11:14 +0200

    Add 1.9: unicafe step 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,6 +9,16 @@ const Statistics = (props) => {
   const average = total === 0 ? 0 : (props.good * 1 + props.neutral * 0 + props.bad * -1) / total;
   const positivePercentage = total === 0 ? 0 : (props.good / total) * 100;
 
+  if(total === 0){
+
+      return (
+        <div>
+            <h1>statistics</h1>
+            <p>No feedback given yet</p>
+        </div>
+      )
+  }
+
   return (
     <div>
       <h1>statistics</h1>
@@ -20,6 +30,8 @@ const Statistics = (props) => {
       <p>positive {positivePercentage}%</p>
     </div>
   );
+
+  
 }
 
 const App = () => {
@@ -27,6 +39,8 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+
 
   return (
     <div>
