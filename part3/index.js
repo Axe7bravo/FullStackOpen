060310688e101491192475c87ffec1fa9742d425
Thato Nkoko <thato.dice@gmail.com>
2025-03-16 01:27:39 +0200

    Add Port adjustment for hosting

--- part3/index.js
+++ part3/index.js
@@ -119,9 +119,9 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
   }
   
-  app.use(unknownEndpoint)
+  app.use(unknownEndpoint);
   
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   });
